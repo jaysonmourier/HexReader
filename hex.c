73e7c012917ace1be8f6a2afc0d989f436dff857
Jayson Mourier <mourierjayson@protonmail.com>
2022-09-01 14:22:17 +0200

    added __start, __stop and loadFile

--- hex.c
+++ hex.c
@@ -1,10 +1,34 @@
 #include <stdio.h>
+#include <time.h>
+#include <assert.h>
 
-void __attribute__((constructor)) __start() { fprintf(stdout, "\n\tJAYSON MOURIER\n\tHEX READER\n"); fflush(stdout); };
+// GLOBAL
+time_t startTime;
+
+void __attribute__((constructor)) __start() 
+{
+    startTime = time( NULL ); 
+    fprintf(stdout, "\n\tJAYSON MOURIER\n\tHEX READER\n");
+    fflush(stdout); 
+};
+
+void __attribute__((destructor)) __stop()
+{
+    fprintf(stdout, "\n%lld\n", time( NULL ) - startTime);
+}
+
+FILE *loadFile(const char *__restrict__ src) __attribute__((nonnull (1)));
 
 int
 main(void)
 {
     fprintf(stdout, "Hex reader\n");
     return 0;
+}
+
+FILE *loadFile(const char *__restrict__ src)
+{
+    FILE *tmp = fopen64(src, "rb+");
+    assert(tmp != NULL);
+    return tmp;
 }
